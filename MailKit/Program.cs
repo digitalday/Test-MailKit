48f4113a5afd4fd8464831b40d601fd5a6021d59
Daniil Slychenyuk <Daniil.Slychenyuk@outlook.com>
2022-04-05 05:54:31 +0600

    Добавьте файлы проекта.

---
+++ MailKit/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using MailKit;
+using MailKit.Net.Smtp;
+using MimeKit;
+
+namespace Mail
+{
+    class program
+    {
+        
+        public static void Main()
+        {
+            MimeMessage message = new MimeMessage();
+            var bodyBuilderHTML = new BodyBuilder();
+
+
+            Console.Write("почта получателя: ");
+            string email = Console.ReadLine();
+
+            if(email.Contains("@") || email.Contains("."))
+            {
+
+                message.From.Add(new MailboxAddress("Digital Days", "mailkittestcsharp@gmail.com")); //отправитель   // почта отправителя
+                message.To.Add(MailboxAddress.Parse(email));//получатель
+                message.ReplyTo.Add(new MailboxAddress("Support", "mailkittestcsharp@gmail.com")); // Загаловок отправителя при ответе на пмсьмо   // почта для ответа на письмо
+                message.Subject = "Welcome!"; // тема 
+
+
+
+
+                // отправка текста в формате HTML
+
+                
+                bodyBuilderHTML.HtmlBody = "<b>Test HTML TEXT</b>"; // пример текста
+                message.Body = bodyBuilderHTML.ToMessageBody(); // передаем наш текст
+
+
+
+
+
+                // создаем и подключаем smtp client
+                SmtpClient smtp = new SmtpClient();
+                try
+                {
+                    
+                    smtp.Connect("smtp.gmail.com", 465, true); // Подключение    1)адресс smpt 2) порт 3) SSL подключение
+                    smtp.Authenticate("mailkittestcsharp@gmail.com", password.Mailpass); // авторизация    1) почта 2) пароль от нее
+                    smtp.Send(message); // отправка 
+                    Console.WriteLine($"Сообщение было отправлено на адресс: {email}");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex);
+                }
+                finally
+                {
+
+                    // Отключение клиента
+                    smtp.Disconnect(true);
+                    smtp.Dispose();
+                }
+
+            } else
+            {
+                Console.WriteLine("Ошибка!");
+            }
+
+        }
+    }
+}
---
+++ MailKit/password.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mail
+{
+    public class password
+    {
+        public static string Mailpass = "Qwe123!@#";
+
+    }
+}
